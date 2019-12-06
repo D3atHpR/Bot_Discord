a6d8c69518cbe2c8e4aad43d9fc54030a6937920
D3atHpR <kastiel103@gmail.com>
2019-12-06 21:30:22 -0200

    Alteração1

--- bot.js
+++ bot.js
@@ -1,2 +1,18 @@
 const Discord = require ("discord.js")
 const config = require("./config.json")
+const bot = new Discord.Client()
+
+
+bot.on("ready",async()=>{
+    console.log("bot online")
+    bot.user.setActivity(`Olá, Tudo bem? | ${config.perfix}Okay!`,{type :'Playing'})
+
+})
+
+bot.on("message", async message=> {
+    if(message.author.bot) return;
+    if(message.channel.type ==="dm") return;
+
+})
+
+bot.login(config.token)
